a3b3bbd396e603bb1536ded9ff11888e45a89c82
Lucas Vinicius <lucasvinicius564@hotmail.com>
2022-06-01 17:26:07 -0300

    implementando fragmentos

--- app/src/main/java/com/example/weserv/MainActivity.java
+++ app/src/main/java/com/example/weserv/MainActivity.java
@@ -5,26 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.weserv.boundary.TelaCategoria;
+import com.example.weserv.boundary.TelaServico;
+import com.example.weserv.control.ControleCliente;
+import com.example.weserv.entity.Cliente;
 
 public class MainActivity extends AppCompatActivity {
+    Cliente cliente = ControleCliente.getCliente(1);
+
+    TextView nomeCliente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nomeCliente = findViewById(R.id.nomeCliente);
+        nomeCliente.setText("Olá, " + cliente.getNome());
     }
 
-    public void pedir(View view){
-        Button buttonPedir;
-
-        buttonPedir = findViewById(R.id.buttonPedir);
-
-        Intent intent = new Intent(MainActivity.this, TelaCategoria.class);
-        startActivity(intent);
-
+    // inicia a intenção de pedir um serviço
+    public void pedirServico(View view){
+        Intent it_telaCategoria = new Intent(MainActivity.this, TelaServico.class);
+        startActivity(it_telaCategoria);
     }
 
 }
--- app/src/main/java/com/example/weserv/control/ControleCategoria.java
+++ app/src/main/java/com/example/weserv/control/ControleCategoria.java
@@ -1,19 +1,24 @@
 package com.example.weserv.control;
 
+import com.example.weserv.boundary.TelaCategoria;
 import com.example.weserv.entity.Categoria;
 
 import java.util.ArrayList;
 
 public class ControleCategoria {
+    private TelaCategoria tc;
 
-
+    public ControleCategoria(TelaCategoria tc){
+        this.tc = tc;
+    }
 
     public ArrayList<Categoria> getCategoria() {
         ArrayList<Categoria> categorias = new ArrayList<>();
 
         categorias.add(new Categoria(1, "Diarista"));
         categorias.add(new Categoria(2, "Pedreiro"));
-        categorias.add(new Categoria(3, "Motorista"));
+        categorias.add(new Categoria(3, "Ar condicionado"));
+        categorias.add(new Categoria(4, "Psicólogo"));
 
         return categorias;
     }
